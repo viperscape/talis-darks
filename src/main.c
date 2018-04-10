ed14b0f96b5d7083dc142734188be57054917709
viperscape <chrisfgill@gmail.com>
2018-04-10 14:59:41 -0400

    handle menu init

--- src/main.c
+++ src/main.c
@@ -3,11 +3,24 @@
 #include "ui.h"
 
 int main() {
-    struct game_screen ui = screen_init();
+    struct game_screen ui = screen_init(main_menu_ui);
 
     int c;
-    while ((c = getchar()) != 'x') {
+    while ((c = getchar()) != 'q') {
         if (c == '\n') continue;
-        printf("ui %i\n", screen_get(&ui));
+
+        switch (screen_get(&ui)) {
+            case game_ui: 
+                printf("enter game\n");
+                break;
+            
+            case main_menu_ui:
+                printf("the choice is yours\n");
+                break;
+
+            default:
+                printf("unhandled menu screen %i\n", screen_get(&ui));
+                break;
+        }
     }
 }
