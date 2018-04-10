b26a31e5be5117785acfd10c3d39da8264bcc658
viperscape <chrisfgill@gmail.com>
2018-04-10 14:35:44 -0400

    add basic loop and ui menu states

--- src/main.c
+++ src/main.c
@@ -1,1 +1,13 @@
-int main() {}
+#include <stdio.h>
+
+#include "ui.h"
+
+int main() {
+    struct game_screen ui = screen_init();
+
+    int c;
+    while ((c = getchar()) != 'x') {
+        if (c == '\n') continue;
+        printf("ui %i\n", screen_get(&ui));
+    }
+}
