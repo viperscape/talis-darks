79f059908ed36f4db1d679c526141f42eb2bbc6d
viperscape <chrisfgill@gmail.com>
2018-04-10 15:21:48 -0400

    wip menu inputs and routing

--- src/main.c
+++ src/main.c
@@ -5,16 +5,28 @@
 int main() {
     struct game_screen ui = screen_init(main_menu_ui);
 
-    int c;
+    int c = -1;
     for(;;) {
-        if (c != '\n')
         switch (screen_get(&ui)) {
-            case game_ui: 
-                printf("enter game\n");
+            case game_ui:
+                if (c == ' ') screen_add(&ui, unit_ui);
+                else if (c == 'b') screen_remove(&ui);
+                else {
+                    printf("entering game\npress q to quit\n");
+                    printf("unit: space\n");
+                    printf("back: b\n");
+                }
+
                 break;
             
             case main_menu_ui:
-                printf("the choice is yours\n");
+                if (c == ' ') screen_add(&ui, game_ui);
+                else {
+                    printf("the choice is yours\n");
+                    printf("start game: space\n");
+                    printf("end game: q\n");
+                }
+                
                 break;
 
             default:
@@ -22,6 +34,7 @@ int main() {
                 break;
         }
 
+        c = -1;
         if ((c = getchar()) == 'q') break;
     }
 }
