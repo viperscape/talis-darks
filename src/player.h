ac8a81461805f21b4ae875158095b8d1092cf3ee
viperscape <chrisfgill@gmail.com>
2018-04-11 14:59:48 -0400

    player init

---
+++ src/player.h
@@ -0,0 +1,21 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+struct player {
+    // physical
+    int strength;
+    int toughness;
+    int endurance;
+    int agility;
+    int dexterity;
+
+    // mental
+    int intellect;
+    int valor;
+    int resolve;
+    int presence;
+    int awareness;
+    int charisma;
+};
+
+#endif
