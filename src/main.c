d7224e6eb3d4080e7d71acf25998faa4113a8aa6
viperscape <chrisfgill@gmail.com>
2018-04-14 17:07:44 -0400

    basic player stats render text

--- src/main.c
+++ src/main.c
@@ -3,12 +3,15 @@
 #include "ui.h"
 #include "font.h"
 #include "gfx.h"
+#include "player.h"
 
 int win_w = 1024;
 int win_h = 768;
 SDL_Renderer* render_ctx = NULL;
-struct font font;
+struct font font, font24;
 struct game_screen menus;
+
+struct player player = { 10, 95, 8, 75, 63, 55, 4, 30, 21, 50, 98 };
 
 int handle_input () {
     SDL_Event event;
@@ -33,6 +36,14 @@ int render_cycle (SDL_Renderer* renderer, float delta) {
         cache_render(renderer, &cache);
         cache_free(&cache);
     }
+    else if (screen_get(&menus) == game_ui) {
+        char stats[256] = {'\0'};
+        sprintf(stats, "player %s\nstr: %i\nagility: %i\nintellect %i\ncharisma %i\n", 
+            "Galendra", player.strength, player.agility, player.intellect, player.charisma);
+        struct cache cache = font_cache(&font24, 50, 50, stats);
+        cache_render(renderer, &cache);
+        cache_free(&cache);
+    }
 
     return 0;
 }
@@ -45,6 +56,7 @@ void game_free() {
 SDL_Renderer* game_init () {
     SDL_Renderer* renderer = gfx_init("Talis Darks", 200, 200, win_w,win_h);
     font = font_init(renderer, "assets/fonts/constantia");
+    font24 = font_init(renderer, "assets/fonts/constantia-24");
 
     return renderer;
 }
